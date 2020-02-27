339ef2ea4e1b94f1819103aa06505080423d794e
Nate Nading <nate.nading@nrccua.org>
2020-02-27 16:18:37 -0600

    RW-9: Added Option to Input Custom Location; Added Error Handling

--- client/src/components/GetLocation/index.js
+++ client/src/components/GetLocation/index.js
@@ -12,14 +12,35 @@ import history from '../../history';
 import { updateSearchInfo } from '../../redux/actions/searchInfo';
 
 class GetLocation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    /**
+     * errorMessage - JSX; if there is an issue with something the user is doing, this message is displayed
+     * locationInputValue - String; the value inside the input that receives a location
+     */
+    this.state = { errorMessage: null, locationInputValue: '' };
+  }
+
   // gets the location using HTML5 geolocation and references a callback function to update the redux state
   getLocation = () => {
-    navigator.geolocation.getCurrentPosition(this.updateSearchInfoWithLatAndLong);
+    navigator.geolocation.getCurrentPosition(this.updateSearchInfoWithLatAndLong, (error) => {
+      if (error.code === error.PERMISSION_DENIED) { // if the function failed because permission was denied, display a custom error message
+        this.setState({ errorMessage: <div>You denied access to your location. Either go into site settings and change your permissions, or input a location.</div>})
+      } else { // if the function failed for a different reason, display the error message
+        this.setState({ errorMessage: <div>{error.message}</div>});
+      }
+    });
   }
 
   // updates the redux state searchInfo by adding a latitude and a longitude
   updateSearchInfoWithLatAndLong = (location) => {
     this.props.updateSearchInfo({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+  }
+
+  // updates the redux state searchInfo by adding a location
+  updateSearchInfoWithProvidedLocation = () => {
+    this.props.updateSearchInfo({ location: this.state.locationInputValue });
   }
 
   componentDidUpdate() {
@@ -35,6 +56,15 @@ class GetLocation extends React.Component {
     return (
       <div>
         <h1>Select a Location or Allow this Site to Access Your Location</h1>
+        {this.state.errorMessage}
+
+        <input type='text' placeholder='Type in a Location' value={this.state.locationInputValue}
+          onChange={(e) => { this.setState({ locationInputValue: e.target.value }); }}></input>
+        <button onClick={() => {
+            // if there is a location provided, record the location; if there is no location provided, then prompt the user for a location
+            this.state.locationInputValue !== '' ? this.updateSearchInfoWithProvidedLocation() : this.setState({ errorMessage: <div>Please provide a location.</div> });
+          }}>Use This Location</button>
+
         <button onClick={this.getLocation}>Get Your Location</button>
       </div>
     );
