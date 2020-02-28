182d0f43967a092c102a0195be8bc79d59c73b47
nate13nate <nate13nate@gmail.com>
2020-02-27 20:32:23 -0600

    Start of Adding Map to Site

---
+++ server/apiTokens.js
@@ -0,0 +1,3 @@
+const yelpFusionToken = 'Mvo2QTUxWOtTekDksTnqmLvdHOTzY28k8UYuH9svFl1RQgCzwbyHy9T2G38MXVhfMv9NEpBDSrbChPKMvVtAh4tw9CCIg6sYD7_koosGyBxQ0iaZLDJP7mLO8OVOXnYx';
+
+module.exports = { yelpFusionToken };
