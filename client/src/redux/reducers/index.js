74646e3e74d8dd5f282a324889077f1a5aa9b8b4
Nate Nading <nate.nading@nrccua.org>
2020-03-04 11:56:25 -0600

    RW-7: Added Sort Option

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -5,15 +5,9 @@ import restaurantsReducer from './restaurantsReducer';
 import searchInfoReducer from './searchInfoReducer';
 import viewedRestaurantReducer from './viewedRestaurantReducer';
 
-// placeholder reducer; returns null
-const fakeReducer = () => {
-  return null;
-}
-
 export default combineReducers({
   pageInfo: pageInfoReducer, // information on the different pages of the restaurant table
   restaurants: restaurantsReducer, // the total list of restaurants pulled from the Yelp API
   searchInfo: searchInfoReducer, // the parameters for searching through the list of restaurants
-  sortInfo: fakeReducer, // the parameters for sorting the list of restaurants
   viewedRestaurant: viewedRestaurantReducer, // the restaurant currently viewed by the user
 });
